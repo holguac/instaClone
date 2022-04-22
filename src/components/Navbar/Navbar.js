70d29ce746f28ea60ce12f22722d846910b09dd3
Holly Christopher <holly_christopher@hotmail.com>
2022-04-22 16:08:33 +0100

    added navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,8 @@
 import "./Navbar.css";
+// import logo from "../../images/logo.png";
+// import profilePic from "../../images/picprofile.jpeg";
 import { BsFillHouseDoorFill } from "react-icons/bs/";
-// import { IoPaperPlaneOutline } from "react-icons/io";
+import { FiSend } from "react-icons/fi";
 import { CgAddR } from "react-icons/cg";
 import { AiOutlineCompass } from "react-icons/ai";
 import { FiHeart } from "react-icons/fi";
@@ -9,21 +11,28 @@ import { FiHeart } from "react-icons/fi";
 const Nav = () => {
 return (
     <div className="nav">
-          <div className="house">
-              <BsFillHouseDoorFill />
+      {/* <img src={logo} alt="instagram logo" /> */}
+      <div className="search">
+        <input placeholder="Search" />
+      </div>
+          <div className="icons">
+            <a href="/">
+            <BsFillHouseDoorFill style={{ color: "black", fontSize: "25px" }} />
+            </a>
+            <a href="/">
+            <FiSend style={{ color: "black", fontSize: "25px" }} />
+            </a>
+            <a href="/">
+            <CgAddR style={{ color: "black", fontSize: "25px" }} />
+            </a>
+            <a href="/">
+            <AiOutlineCompass style={{ color: "black", fontSize: "25px" }} />
+            </a>
+            <a href="/">
+            <FiHeart style={{ color: "black", fontSize: "25px" }} />
+            </a>
               </div>
-              {/* <div className="plane">
-              <IoPaperPlaneOutline />
-              </div> */}
-              <div className="add">
-              <CgAddR />
-              </div>
-              <div className="compass">
-              <AiOutlineCompass />
-              </div>
-              <div className="heart">
-              <FiHeart />
-              </div>
+              {/* <img className="pic" src={profilePic } alt="profile pic" /> */}
     </div>
   )
 };
